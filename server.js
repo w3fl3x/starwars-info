e350c5911b29c7664962f1d4cf1abfb364069de8
Huy Nguyen <huy.m.nguyen@live.com>
2019-08-14 20:19:33 -0700

    display character

--- server.js
+++ server.js
@@ -1,63 +1,81 @@
 // Dependencies
-// ===========================================================
 var express = require("express");
 
 var app = express();
 var PORT = 3000;
 
+/// client REQUEST
+
+/// middleware - sits between client request and server receiving request
+
+/// receives REQUEST
+
+// Sets up the Express app to handle data parsing --- middleware telling how to handle a different client request
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Data
-// ===========================================================
-var characters = [{
-  routeName: "yoda",
-  name: "Yoda",
-  role: "Jedi Master",
-  age: 900,
-  forcePoints: 2000
-}, {
-  routeName: "darthmaul",
-  name: "Darth Maul",
-  role: "Sith Lord",
-  age: 200,
-  forcePoints: 1200
-}, {
-  routeName: "obiwankenobi",
-  name: "Obi Wan Kenobi",
-  role: "Jedi Master",
-  age: 55,
-  forcePoints: 1350
-}];
+var characters = [
+  {
+    routeName: "yoda",
+    name: "Yoda",
+    role: "Jedi Master",
+    age: 900,
+    forcePoints: 2000
+  },
+  {
+    routeName: "darthmaul",
+    name: "Darth Maul",
+    role: "Sith Lord",
+    age: 200,
+    forcePoints: 1200
+  },
+  {
+    routeName: "obiwankenobi",
+    name: "Obi Wan Kenobi",
+    role: "Jedi Master",
+    age: 55,
+    forcePoints: 1350
+  }
+];
 
 // Routes
-// ===========================================================
-
 app.get("/", function(req, res) {
   res.send("Welcome to the Star Wars Page!");
 });
 
-// What does this route do? --- regular get route to bring back characters... show all characters
+// Displays all characters
 app.get("/api/characters", function(req, res) {
   return res.json(characters);
 });
 
-// What does this route do? --- dynamic route that pulls out one character that we want
+// Displays a single character, or shows "No character found"
 app.get("/api/characters/:character", function(req, res) {
-  // What does this code do? --- pulls out what was in the character and printing in the browser
   var chosen = req.params.character;
+
   console.log(chosen);
 
-  // What does this code do? --- looping through character array, if check to see what user typed to see if match a character, then return with a json response for character
   for (var i = 0; i < characters.length; i++) {
     if (chosen === characters[i].routeName) {
       return res.json(characters[i]);
     }
   }
 
-  // What does this code do?
   return res.send("No character found");
+
 });
 
-// Listener
-// ===========================================================
+// Create New Characters - takes in JSON input
+app.post("/api/characters", function(req, res) {
+  var newCharacter = req.body;
+
+  console.log(newCharacter);
+
+  characters.push(newCharacter);
+
+  res.json(newCharacter);
+});
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
